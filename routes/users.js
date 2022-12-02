1c675ba0baa25857795c414aa1d7a6f90b50c543
adamECE <85218358+adamECE@users.noreply.github.com>
2022-12-02 14:14:09 -0700

    Update users.js
    added deviceKey

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   username: String,
-  password: String
+  password: String,
+  deviceKey: String
 })
 
 const user = mongoose.model("User", UserSchema, "users");
