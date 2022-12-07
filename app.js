cf500703ed1caaf6e0f0e9d4f3f620972ef8d0b8
adamECE <85218358+adamECE@users.noreply.github.com>
2022-12-07 11:12:53 -0700

    Update app.js
    added JWTauth

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 /* 
-  Installed body-parser, mongoose
+  Installed body-parser, mongoose, generate-api-key
+  npm install dotenv --save
+  npm i jsonwebtoken bcryptjs
+  npm i cookie-parser
 */
 
 const express = require('express');
@@ -7,42 +10,28 @@ const path = require('path');
 const bodyParser = require('body-parser')
 const PORT = 5500; 
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
 
-mongoose.connect('mongodb+srv://ahoffmei:ece413@ece413cluster.fetwdvy.mongodb.net/ece413final?retryWrites=true&w=majority').then(() => console.log("connected to mongodb server")
+
+
+mongoose.connect(process.env.MONGODB_CONNECT).then(() => console.log("connected to mongodb server")
 ).catch((err) => console.log("error connecting to cluster: " + err)); 
 
 //const indexRouter = require('./routes/index');
 
 const user = require('./routes/users');
+const apiKeys = require('./routes/apiKey');
+//const hrdata = require('./routes/heartRateData')
+
 const mainRouter = require('./routes/router');
 const { kStringMaxLength } = require('buffer');
-
-
-const UserSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  deviceKey: String, 
-  
-})
-
-const accountSchema = new mongoose.Schema({
-  username: String,
-  password: String
-})
-
-
-//const user = mongoose.model("User", UserSchema, "users");
-
-user.findOne({username:"hi"}, (err, foundItem) => {
-  if(err){
-    console.log("Error:")
-    console.log(err);
-  } else {
-    console.log("Item found: "+foundItem);
-  }
-})
+const { time } = require('console');
 
 const app = express();
+app.use(cookieParser()); 
 
 app.use('/', express.static(path.join(__dirname, 'public/frontend')));
 app.use(bodyParser.json());
@@ -53,14 +42,22 @@ app.post('/api/login', async (req, res) => {
 
   try {
     const response = await user.findOne({
-      username,
-      password 
+      username
     })
     console.log(response);
     if (response==null){
       return res.status(401).json({message: 'Unsuccessful login'}); 
     } else {
-      return res.status(200).json({message: 'Successful login'}); 
+      const passwordMatches = await bcrypt.compare(password,response.password);
+      if(!passwordMatches){
+        return res.json({message:'Wrong credentials pass'});
+      }
+      const token = await jwt.sign({ id: userExist._id }, process.env.SECRET_KEY, {
+        expiresIn: process.env.JWT_EXPIRE,
+      });
+      
+      return res.cookie({"token":token}).json({success:true,message:'LoggedIn Successfully'})
+      //return res.status(200).json({message: 'Successful login'}); 
     }
   } catch (err) {
     console.log(err);
@@ -68,16 +65,27 @@ app.post('/api/login', async (req, res) => {
   }
 })
  
-app.post('/api/register', async (req, res) => {
-  console.log(req.body); 
+app.post('/api/register/', async (req, res) => {
+  console.log(req.body);
+  console.log("apiKey: " + req.params.apiKey); 
+  
   const {username, password, deviceKey} = req.body; 
-
+  const heartRates = []; 
+  const apiKey = "16b43zQHSiX0e0ZfT8q83Xxq6elsd5Zv";
   try {
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashPassword;
     const response = await user.create({
       username, 
       password, 
-      deviceKey
+      deviceKey,
+      apiKey, 
+      heartRates 
     })
+    const token = await jwt.sign({ id: user._id }, process.env.SECRET_KEY, {
+      expiresIn: process.env.JWT_EXPIRE,
+  });
   } catch (err) {
     if (err.code === 11000){
       return res.json({status: 'error', error: 'Username already in use'});
@@ -88,27 +96,28 @@ app.post('/api/register', async (req, res) => {
   res.json({ status:'ok' })
 })
 
-app.post('/api/testHeartRate', async (req, res) => {
+app.post('/api/testHeartRates/', async (req, res) => {
   console.log(req.body); 
-  const {deviceKey, heartRates} = req.body; 
-
-  try {
-    const response = await user.findOne({deviceKey});
-    if (response != null){
-      await user.findOneAndUpdate(
-        {  
-          deviceKey: deviceKey
-        },
+  
+  const {deviceKey, time, hr1} = req.body; 
+  if(apiKeys.includes(req.query.apiKey)){
+    console.log("if statement entered")
+    try {
+      const response = await user.findOne({deviceKey});
+      const toUpdate = await user.findByIdAndUpdate(
+        response._id, 
         {
-          $push: {
-            heartRates: heartRates
+          "$push": {"heartRates": {
+            time: time, 
+            data: hr1
           }
-        }) 
-    }
-  } catch (err) {
+        }
+        }); 
+    } catch (err) {
+      console.log(err);
       return res.json({status: 'error', error: err});
+    }
   }
-
   res.json({ status:'ok' })
 })
 
