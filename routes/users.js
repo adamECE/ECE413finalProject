baf774409a12219c8a2c4a2fb3b8bef58a44f022
adamECE <85218358+adamECE@users.noreply.github.com>
2022-12-08 23:12:01 -0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,8 @@ const UserSchema = new mongoose.Schema({
   username: String,
   password: String,
   email: String,
-  deviceKey: String
+  deviceKey: String,
+  heartRates: [{time: String, heartRate: String, satOxygen: String}]
 })
 
 const user = mongoose.model("User", UserSchema, "users");
